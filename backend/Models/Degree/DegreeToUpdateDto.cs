f53a644f71df670b10955a815ad3021e4722666f
Baltazar-GC <baltazargarciacanllo@gmail.com>
2024-01-10 02:05:00 -0300

    Degrees Service Improvements

--- backend/Models/Degree/DegreeToUpdateDto.cs
+++ backend/Models/Degree/DegreeToUpdateDto.cs
@@ -1,9 +1,12 @@
-﻿using backend.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using backend.Enums;
 
 namespace backend.Models.Degree
 {
     public class DegreeToUpdateDto
     {
+        [Required]
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public DegreeLevel DegreeLevel { get; set; }
     }
--- backend/Repositories/Interfaces/IDegreeRepository.cs
+++ backend/Repositories/Interfaces/IDegreeRepository.cs
@@ -4,11 +4,14 @@ namespace backend.Repositories.Interfaces
 {
     public interface IDegreeRepository
     {
-        bool AddDegree(Degree newDegree);
-        Degree? GetDegree(int degreeId);
-        bool IsDegree(int degreeId);
-        ICollection<Degree>? GetDegrees();
-        bool UpdateDegree();
-        bool DeleteDegree(int deletedDegreeId);
+        Task<bool> Add(Degree newDegree);
+
+        Task<Degree?> Get(int degreeId);
+
+        Task<ICollection<Degree>> List();
+
+        Task<bool> Update(Degree updatedDegreee);
+
+        Task<bool> Delete(int deletedDegreeId);
     }
 }
